9cbb66098cfc283f3f3e555a8842ad597ae9a2ef
MartinGarrix <martingarrixedm01@gmail.com>
2024-02-27 18:27:59 +0530

    Casino and Homepage

---
+++ Test Suites/HomePage.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>HomePage</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>6215305c-8ef7-4234-b7cd-6d3525c87d31</testSuiteGuid>
+   <testCaseLink>
+      <guid>55d270f4-5e0d-476b-b8a0-ed7162839592</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/HomePage/Open recommended games successful</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
