95d0bc5fb7bdce9d0c024b8f01fab264242c3bb6
Meganathan2905 <meganathan@cloudrevelinnovation.com>
2024-02-29 17:17:11 +0530

    my push

---
+++ Object Repository/Login Page/User not found. Not to worry, you can try.rs
@@ -0,0 +1,97 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>User not found. Not to worry, you can try</name>
+   <tag></tag>
+   <elementGuidId>0b31170b-4c0f-42f4-8bca-c6d22c968715</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>#showMessage > p</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='showMessage']/p</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>p</value>
+      <webElementGuid>2cc900fa-9f81-4cf9-80e3-8fafdb5f3d4d</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value> User not found. Not to worry, you can try again or you can
+                              reset your password or contact us.
+                           </value>
+      <webElementGuid>d7a4437c-e1b4-425a-9373-1bacde94874a</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;showMessage&quot;)/p[1]</value>
+      <webElementGuid>c923ebc1-fe34-4299-b094-8e9ed0797b86</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='showMessage']/p</value>
+      <webElementGuid>57fcd862-df55-4e3c-804e-ff249662f473</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='OK'])[1]/following::p[1]</value>
+      <webElementGuid>d4678b6e-7292-458c-98ac-e190a6f04bf2</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Success'])[1]/following::p[2]</value>
+      <webElementGuid>02152a99-ce8e-4f89-b252-729d64a342ca</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Not to worry, you can try again or you can']/parent::*</value>
+      <webElementGuid>44cf592d-7a48-4136-882d-1a53e4b011bf</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div/div/div/div/div/p</value>
+      <webElementGuid>99181294-a8a5-4250-bb10-e8cc1670b3ba</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//p[(text() = ' User not found. Not to worry, you can try again or you can
+                              reset your password or contact us.
+                           ' or . = ' User not found. Not to worry, you can try again or you can
+                              reset your password or contact us.
+                           ')]</value>
+      <webElementGuid>89814eb4-b272-48aa-a2a7-9131c11a45e6</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
